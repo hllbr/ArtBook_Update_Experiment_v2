eb95710f42217dc28d565e43030382069872d400
hllbr <halibrahim.kocak@gmail.com>
2021-07-31 19:59:48 +0300

    LandmarkBook_Update_2021_version

---
+++ app/src/main/java/com/hllbr/artbook_update_experiment_v2/Art.java
@@ -0,0 +1,11 @@
+package com.hllbr.artbook_update_experiment_v2;
+
+public class Art {
+    String name;
+    int id;
+    public Art(String name,int id){
+        this.id = id;
+        this.name = name;
+
+    }
+}
